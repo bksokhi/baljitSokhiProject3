74731aeec2ea0240b9283c282409f0661e222b67
Baljit <sokhibaljit@gmail.com>
2020-09-12 22:53:39 -0400

    radio buttons js

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     $('.wallForm').on('submit', function (event) {
         event.preventDefault();
 
-        let userInput = $('input').val();
+        let userInput = $('input[name=wallColor]').val();
         
         $('.wall').css({ "background": `${userInput}` });
 
@@ -26,21 +26,46 @@ $(document).ready(function () {
         // if (countOfLi >= 3) {
         //     $('li:first').text(`${userInput}`);
         // } 
-        $('input').val('');
+        $('input[name=wallColor').val('');
     })
     
     // ● Furniture shade radio buttons, create event listener on change
     // ● Target furniture css selectors via background - color to change color upon button
     // click
 
-    $('.furnitureForm').on('change', function () {
-        
-    })
+    $('.furnitureForm input').on('click', function () {
+        let furnitureShade = $('input[name=furnitureShade]:checked').val();
+
+        if (furnitureShade == 'furnitureLight') {
+            $('.left, .right, .middle, .bottomLeft, .bottomRight, .tableTop, .tableFront, .tableLeftLeg, .tableRightLeg').css({ "background": 'grey' });
+
+        } else if (furnitureShade == 'furnitureMedium') {
+            $('.left, .right, .middle, .bottomLeft, .bottomRight, .tableTop, .tableFront, .tableLeftLeg, .tableRightLeg').css({ "background": 'brown' });
+
+        } else if (furnitureShade == 'furnitureDark') {
+            $('.left, .right, .middle, .bottomLeft, .bottomRight, .tableTop, .tableFront, .tableLeftLeg, .tableRightLeg').css({ "background": 'black' });
+        }
+
+    });
+
+    // ● Flooring radio buttons, create event listener on change
+    // ● Target furniture css selectors via background - color to change color upon button
+    // click
+
+    $('.floorForm input').on('click', function () {
+        let floorShade = $('input[name=floorShade]:checked').val();
+
+        if (floorShade == 'floorLight') {
+            $('.floor').css({ "background": '#f2e3b3' })
+            
+        } else if (floorShade == 'floorMedium') {
+            $('.floor').css({ "background": 'burlywood' })
+
+        } else if (floorShade == 'floorDark') {
+            $('.floor').css({ "background": '#4a3d24'})
+        }
+    });
 
 
 });
 
-
-// ● Flooring radio buttons, create event listener on change
-// ● Target furniture css selectors via background - color to change color upon button
-// click
